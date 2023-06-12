1138c3cdf4596c2ba684cece11e886143378c690
User <greenzeb@inbox.lv>
2023-06-12 16:41:59 +0300

    add files

---
+++ WalkTracker/WalkTracker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WalkTracker.ViewModels;
+using Xamarin.Forms;
+
+namespace WalkTracker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ WalkTracker/WalkTracker/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using WalkTracker.Models;
+using WalkTracker.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WalkTracker.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
